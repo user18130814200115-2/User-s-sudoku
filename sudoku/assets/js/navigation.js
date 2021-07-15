d65f3958f9e6274673292fd0e82fdbba4dca3de7
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-07-15 21:42:57 +0200

    Update navigation.js
    explanation comments

--- sudoku/assets/js/navigation.js
+++ sudoku/assets/js/navigation.js
@@ -1,12 +1,12 @@
-var selected = 0;
+var selected = 0; //set selected cell to the top left initially
 
 window.onload = function () {
-	Focus();
+	Focus(); //focus the first cell
 }
 
 
 function Focus() {
-	selected = Math.min(Math.max(parseInt(selected), 0), 80);
+	selected = Math.min(Math.max(parseInt(selected), 0), 80); //clamp the cell number becasue there are only 80 cells
 	var e = document.getElementById("cell-"+selected);
  	e.focus();
  	var val = e.value; //store the value of the element
@@ -15,6 +15,7 @@ function Focus() {
 }
 
 function Illeagal() {
+	//check if the user's input is a number between 1 and 9
 	setTimeout(Check, 1);
 	var e = document.getElementById("cell-"+selected);
 	e.value = e.value.slice(-1);
@@ -23,22 +24,22 @@ function Illeagal() {
 
 document.onkeypress = function(e) {
 	if (e.keyCode == 37) {
-		selected += -1;
+		selected += -1; //left
 	}
 	else if (e.keyCode == 38) {
-		selected += -9;
+		selected += -9; //up
 	}
 	else if (e.keyCode == 39) {
-		selected += 1;
+		selected += 1; //right
 	}
 	else if (e.keyCode == 40) {
-		selected += 9;
+		selected += 9; //down
 	}
 	if (e.key == "SoftLeft" && CurrentSudokuIndex > 0) {
-		Load(CurrentSudokuIndex-1);
+		Load(CurrentSudokuIndex-1); //load the previous sudoku in the array (in load.js)
 	}
 	else if (e.key == "SoftRight" && CurrentSudokuIndex < 100) {
-		Load(CurrentSudokuIndex+1);
+		Load(CurrentSudokuIndex+1); // load the next sudoku in the array (in load.js)
 	}
 	else {
 		setTimeout(Focus, 1);
